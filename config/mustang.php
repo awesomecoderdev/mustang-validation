c1c4aeffd4125a5e7a9ae611dfb4e2bd7cf5f90f
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-26 17:33:13 +0600

    Startup Plugin

--- config/mustang.php
+++ config/mustang.php
@@ -17,4 +17,17 @@ return [
 
     'name' => __("Mustang Validation", "mustang-validation"),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Plugin Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your plugin. This value is used when the
+    | framework needs to place the plugin's name in a notification or
+    | any other location as required by the plugin or its packages.
+    |
+    */
+
+    'text-domain' => "mustang-validation",
+
 ];
