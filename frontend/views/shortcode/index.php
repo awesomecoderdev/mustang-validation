d616a2aab8ac3f366f5a5a214a896d6be8a062e2
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-30 18:15:10 +0600

    Startup Plugin

--- frontend/views/shortcode/index.php
+++ frontend/views/shortcode/index.php
@@ -1,1 +1,3 @@
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore iste voluptatibus eaque architecto tempora rerum impedit a ad pariatur, obcaecati, omnis voluptatem! Ipsam, laborum totam. Quo, voluptatum. Dolorem, aspernatur.</p>
+
+<button>Mustang</button>
